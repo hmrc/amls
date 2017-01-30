9d5b504e49f25cf4aeddf9f4d0cd6d687734f3d6
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-01-30 16:04:00 +0000

    AMLS-1233: WIP - added tests for backwards compatability with old json structure

--- test/models/des/aboutyou/AboutYouSpec.scala
+++ test/models/des/aboutyou/AboutYouSpec.scala
@@ -16,9 +16,9 @@
 
 package models.des.aboutyou
 
-import models.fe.declaration.{Other, ExternalAccountant, BeneficialShareholder}
+import models.fe.declaration.{BeneficialShareholder, ExternalAccountant, Other}
 import org.scalatestplus.play.PlaySpec
-import play.api.libs.json.Json
+import play.api.libs.json.{JsSuccess, Json}
 
 class AboutYouSpec extends PlaySpec {
   "AboutYouDetails" must {
@@ -40,6 +40,125 @@ class AboutYouSpec extends PlaySpec {
       val FEaboutyouModel = models.fe.declaration.AddPerson("fName", Some("middlename"), "lName", BeneficialShareholder)
       val aboutyouModel = Aboutyou(Some(IndividualDetails("fName", Some("middlename"), "lName")), true, Some("Beneficial Shareholder"), None, Some("Other"), None)
       Aboutyou.convert(FEaboutyouModel) must be(aboutyouModel)
+    }
+
+    "be convertible from FE request to DES for roleForTheBusiness" in {
+      val FEaboutyouModel = models.fe.declaration.AddPerson("fName", None, "lName", ExternalAccountant)
+      val aboutyouModel = Aboutyou(Some(IndividualDetails("fName", None, "lName")), false, None, None, Some("External Accountant"), None)
+      Aboutyou.convert(FEaboutyouModel) must be(aboutyouModel)
+    }
+
+    "be convertible from FE request to DES for OtherroleWithinBusiness" in {
+      val FEaboutyouModel = models.fe.declaration.AddPerson("fName", None, "lName", Other("Agent"))
+      val aboutyouModel = Aboutyou(Some(IndividualDetails("fName", None, "lName")), false, None, None, Some("Other"), Some("Agent"))
+      Aboutyou.convert(FEaboutyouModel) must be(aboutyouModel)
+    }
+  }
+}
+
+class AboutYouRelease7Spec extends PlaySpec {
+
+  "AboutYouRelease7" when {
+    "given the release 7 json" must {
+      "be serialisable for roleWithinBusiness" in {
+
+        val json = Json.obj(
+          "individualDetails" -> Json.obj("firstName" -> "fName", "lastName" -> "lName"),
+          "employedWithinBusiness" -> true,
+          "roleWithinBusiness" -> Json.obj(
+            "beneficialShareholder" -> true,
+            "director" -> true,
+            "partner" -> true,
+            "internalAccountant" -> true,
+            "soleProprietor" -> true,
+            "nominatedOfficer" -> true,
+            "designatedMember" -> true,
+            "other" -> false
+          )
+        )
+
+        val aboutyouModel = AboutYouRelease7(
+          Some(IndividualDetails("fName", None, "lName")),
+          true,
+          Some(RolesWithinBusiness(true, true, true, true, true, true, true, false, None))
+        )
+
+        AboutYouRelease7.format.writes(aboutyouModel) must be(json)
+        AboutYouRelease7.format.reads(json) must be(JsSuccess(aboutyouModel))
+
+      }
+
+      "be serialisable for roleForTheBusiness" in {
+
+        val json = Json.obj(
+          "individualDetails" -> Json.obj("firstName" -> "fName", "lastName" -> "lName"),
+          "employedWithinBusiness" -> false,
+          "roleForTheBusiness" -> Json.obj(
+            "externalAccountant" -> true,
+            "other" -> false
+          )
+        )
+
+        val aboutyouModel = AboutYouRelease7(Some(IndividualDetails("fName", None, "lName")), false, None, Some(RoleForTheBusiness(true, false, None)))
+
+        AboutYouRelease7.format.writes(aboutyouModel) must be(json)
+        AboutYouRelease7.format.reads(json) must be(JsSuccess(aboutyouModel))
+
+      }
+
+    }
+
+    "given the PRE release 7 json" must {
+      "be able to read for roleWithinBusiness" in {
+
+        val json = Json.obj(
+          "individualDetails" -> Json.obj("firstName" -> "fName", "lastName" -> "lName"),
+          "employedWithinBusiness" -> true,
+          "roleWithinBusiness" -> "Beneficial Shareholder"
+        )
+
+        val aboutyouModel = AboutYouRelease7(
+          Some(IndividualDetails("fName", None, "lName")),
+          true,
+          Some(RolesWithinBusiness(true, true, true, true, true, true, true, false, None))
+        )
+
+        AboutYouRelease7.format.reads(json) must be(JsSuccess(aboutyouModel))
+
+      }
+
+      "be able to read for roleForTheBusiness" in {
+
+        val json = Json.obj(
+          "individualDetails" -> Json.obj("firstName" -> "fName", "lastName" -> "lName"),
+          "employedWithinBusiness" -> false,
+          "roleForTheBusiness" -> "External Accountant"
+        )
+
+        val aboutyouModel = AboutYouRelease7(Some(IndividualDetails("fName", None, "lName")), false, None, Some(RoleForTheBusiness(true, false, None)))
+
+        AboutYouRelease7.format.reads(json) must be(JsSuccess(aboutyouModel))
+      }
+
+      // does it really need to write??  only the reads should be different?
+    }
+
+
+
+    "be convertible from FE request to DES for roleWithinBusiness" in {
+//      val FEaboutyouModel = models.fe.declaration.AddPerson("fName", Some("middlename"), "lName", BeneficialShareholder)
+//
+//
+//
+//      val aboutyouModel = AboutYouRelease7(
+//        Some(IndividualDetails("fName", None, "lName")),
+//        true,
+//        Some(RolesWithinBusiness(true,true,false,false,false,false,false, false, None))
+//      )
+//
+//
+//
+//      Aboutyou.convert(FEaboutyouModel) must be(aboutyouModel)
     }
 
     "be convertible from FE request to DES for roleForTheBusiness" in {
