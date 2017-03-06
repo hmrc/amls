430c1429e72892dddc3d6181ead69a8b4f063231
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-03-06 10:49:24 +0000

    add test to nadle exception and update approute for withdrawal

--- test/controllers/WithdrawSubscriptionControllerSpec.scala
+++ test/controllers/WithdrawSubscriptionControllerSpec.scala
@@ -1,37 +1,58 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import connectors.WithdrawSubscriptionConnector
+import exceptions.HttpStatusException
 import models.des.WithdrawSubscriptionResponse
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.mockito.Matchers._
 import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures
 import play.api.libs.json.{JsNull, JsValue, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{contentAsJson, _}
 
 import scala.concurrent.Future
 
-class WithdrawSubscriptionControllerSpec extends PlaySpec with MockitoSugar {
+class WithdrawSubscriptionControllerSpec extends PlaySpec with MockitoSugar with ScalaFutures {
 
   trait Fixture {
+
     object withdrawSubscriptionController extends WithdrawSubscriptionController {
       private[controllers] override val withdrawSubscriptionConnector = mock[WithdrawSubscriptionConnector]
     }
+
   }
 
   val amlsRegistrationNumber = "XAML00000567890"
   val success = WithdrawSubscriptionResponse("2016-09-17T09:30:47Z")
-  val inputRequest = Json.obj("acknowledgementReference" -> "AEF7234BGG12539GH143856HEA123412",
+
+  private val inputRequest = Json.obj("acknowledgementReference" -> "AEF7234BGG12539GH143856HEA123412",
     "withdrawalDate" -> "2015-08-23",
     "withdrawalReason" -> "Other, please specify",
     "withdrawalReasonOthers" -> "Other Reason")
 
-  val postRequest = FakeRequest("POST", "/")
+  private val postRequest = FakeRequest("POST", "/")
     .withHeaders("CONTENT_TYPE" -> "application/json")
     .withBody[JsValue](inputRequest)
 
-  val postRequestWithNoBody = FakeRequest("POST", "/")
+  private val postRequestWithNoBody = FakeRequest("POST", "/")
     .withHeaders("CONTENT_TYPE" -> "application/json")
     .withBody[JsValue](JsNull)
 
@@ -41,25 +62,38 @@ class WithdrawSubscriptionControllerSpec extends PlaySpec with MockitoSugar {
       when(withdrawSubscriptionController.withdrawSubscriptionConnector.withdrawal(any(), any())
       (any(), any(), any())).thenReturn(Future.successful(success))
 
-      val result = withdrawSubscriptionController.withdrawal(amlsRegistrationNumber)(postRequest)
-       status(result) must be(OK)
-       contentAsJson(result) must be(Json.toJson(success))
+      private val result = withdrawSubscriptionController.withdrawal(amlsRegistrationNumber)(postRequest)
+      status(result) must be(OK)
+      contentAsJson(result) must be(Json.toJson(success))
     }
 
 
     "successfully return failed response on invalid request" in new Fixture {
-      val response = Json.obj("errors" -> Seq (
-        Json.obj("path" ->"obj.withdrawalReason",
-        "error" ->"error.path.missing"),
-        Json.obj("path" ->"obj.acknowledgementReference",
-        "error" ->"error.path.missing"),
-        Json.obj("path" ->"obj.withdrawalDate",
-        "error" ->"error.path.missing"))
+      private val response = Json.obj("errors" -> Seq(
+        Json.obj("path" -> "obj.withdrawalReason",
+          "error" -> "error.path.missing"),
+        Json.obj("path" -> "obj.acknowledgementReference",
+          "error" -> "error.path.missing"),
+        Json.obj("path" -> "obj.withdrawalDate",
+          "error" -> "error.path.missing"))
       )
 
-      val result = withdrawSubscriptionController.withdrawal(amlsRegistrationNumber)(postRequestWithNoBody)
+      private val result = withdrawSubscriptionController.withdrawal(amlsRegistrationNumber)(postRequestWithNoBody)
       status(result) must be(BAD_REQUEST)
       contentAsJson(result) must be(response)
+    }
+
+
+    "return failed response on exception" in new Fixture {
+      when(withdrawSubscriptionController.withdrawSubscriptionConnector.withdrawal(any(), any())
+      (any(), any(), any())).thenReturn(Future.failed(HttpStatusException(INTERNAL_SERVER_ERROR, Some("message"))))
+
+      whenReady(withdrawSubscriptionController.withdrawal(amlsRegistrationNumber)(postRequest).failed) {
+        case HttpStatusException(status, body) =>
+          status must be(INTERNAL_SERVER_ERROR)
+          body must be(Some("message"))
+      }
+
     }
   }
 }
