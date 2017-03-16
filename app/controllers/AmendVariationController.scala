6cff0680d7dc42e4e31d4d73b8ede70f66b001bc
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-03-16 11:50:56 +0000

    revert DI changes for AMendVariationController

--- app/controllers/AmendVariationController.scala
+++ app/controllers/AmendVariationController.scala
@@ -97,13 +97,10 @@ trait AmendVariationController extends BaseController {
         Logger.debug(s"$prefix - AmlsRegistrationNumber: $amlsRegistrationNumber")
         update(amlsRegistrationNumber, Variation, RequestType.Variation)
     }
+}
 
-  def renewal(accountType: String, ref: String, amlsRegistrationNumber: String) =
-    Action.async(parse.json) {
-      implicit request =>
-        val prefix = "[AmendVariationController][variation]"
-        Logger.debug(s"$prefix - AmlsRegistrationNumber: $amlsRegistrationNumber")
-        update(amlsRegistrationNumber, Renewal, RequestType.Renewal)
-    }
+object AmendVariationController extends AmendVariationController {
+  // $COVERAGE-OFF$
+  override private[controllers] val service = AmendVariationService
 }
 
