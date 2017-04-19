6bde9818fa6e0f80fe749148fb27779ab239074c
adam-coles <adam.coles@digital.hmrc.gov.uk>
2017-04-19 17:26:22 +0100

    separated fe and des models

---
+++ app/models/fe/AmendVariationResponse.scala
@@ -0,0 +1,27 @@
+package models.fe
+
+import play.api.libs.json.Json
+
+case class AmendVariationResponse(
+                                   processingDate: String,
+                                   etmpFormBundleNumber: String,
+                                   registrationFee: BigDecimal,
+                                   fpFee: Option[BigDecimal],
+                                   fpFeeRate: Option[BigDecimal],
+                                   premiseFee: BigDecimal,
+                                   premiseFeeRate: Option[BigDecimal],
+                                   totalFees: BigDecimal,
+                                   paymentReference: Option[String],
+                                   difference: Option[BigDecimal],
+                                   addedResponsiblePeople: Int = 0,
+                                   addedResponsiblePeopleFitAndProper: Int = 0,
+                                   addedFullYearTradingPremises: Int = 0,
+                                   halfYearlyTradingPremises: Int = 0,
+                                   zeroRatedTradingPremises: Int = 0
+                                 )
+
+object AmendVariationResponse {
+
+  implicit val format = Json.format[AmendVariationResponse]
+
+}
