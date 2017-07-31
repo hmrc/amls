77d0328dafed8a3f221d0d7ff83c408bacf9e3dc
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-07-31 12:42:10 +0100

    Added new model for returning registration details to front-end

---
+++ app/models/fe/registrationdetails/RegistrationDetails.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.fe.registrationdetails
+
+import models.des.registrationdetails.{Individual, Organisation, RegistrationDetails => DesRegistrationDetails}
+
+case class RegistrationDetails(companyName: String, isIndividual: Boolean)
+
+object RegistrationDetails {
+  implicit def convert(details: DesRegistrationDetails) = details match {
+    case DesRegistrationDetails(x@true, details: Individual) => RegistrationDetails(s"${details.firstName} ${details.lastName}", x)
+    case DesRegistrationDetails(x@false, details: Organisation) => RegistrationDetails(details.organisationName, x)
+  }
+}
