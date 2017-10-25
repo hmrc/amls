3e4760d3dbb6b863fd0b49b3cde536526ef0fc37
Steve Hobbs <elkdanger@gmail.com>
2017-10-25 10:56:16 +0100

    Updated platform libraries

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -59,7 +59,6 @@ trait MicroService {
     .settings(scalaSettings: _*)
     .settings(publishingSettings: _*)
     .settings(defaultSettings(): _*)
-    .settings(scalaVersion := "2.11.11")
     .settings(
       libraryDependencies ++= appDependencies,
       retrieveManaged := true,
