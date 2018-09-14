fdb684736287f00d154940e9c1660d6b6b4feed5
sam zaroug <sam.zaroug@digital.hmrc.gov.uk>
2018-09-14 12:14:51 +0100

    AMLS-4288 | AMLS-4299 | Creates failing phase2 unit tests

--- test/models/des/responsiblepeople/ResponsiblePersonsSpec.scala
+++ test/models/des/responsiblepeople/ResponsiblePersonsSpec.scala
@@ -29,6 +29,48 @@ class ResponsiblePersonsSpec extends PlaySpec with OneAppPerSuite {
 
   implicit override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.phase-2-changes" -> false))
 
+  "ResponsiblePersons" should {
+
+    "Serialise to json successfully" in {
+      ResponsiblePersons.jsonWrites.writes(RPValues.model) must be(RPValues.jsonExpectedFromWrite)
+    }
+
+    "Deserialise from json successfully" in {
+      ResponsiblePersons.jsonReads.reads(RPValues.jsonExpectedFromWrite) must be (JsSuccess(RPValues.model))
+    }
+
+    "convert FE model to DES model" in {
+      val responsiblePerson = ResponsiblePersons.convertResponsiblePeopleToResponsiblePerson(RPValues.responsiblePeople, RPValues.businessMatching)
+
+      responsiblePerson must be (RPValues.model)
+    }
+  }
+}
+
+class ResponsiblePersonsPhase2Spec extends PlaySpec with OneAppPerSuite {
+
+  implicit override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.phase-2-changes" -> true))
+
+  "ResponsiblePersonsPhase2" should {
+
+    "Serialise to phase2 json successfully" in {
+      ResponsiblePersons.jsonWrites.writes(RPValues.modelPhase2) must be(RPValues.jsonExpectedFromWritePhase2)
+    }
+
+    "Deserialise from phase2 json successfully" in {
+      ResponsiblePersons.jsonReads.reads(RPValues.jsonExpectedFromWritePhase2) must be (JsSuccess(RPValues.modelPhase2))
+    }
+
+    "convert FE model to DES model for phase 2" in {
+      val responsiblePersonPhase2 = ResponsiblePersons.convertResponsiblePeopleToResponsiblePerson(RPValues.responsiblePeople, RPValues.businessMatching)
+
+      responsiblePersonPhase2 must be (RPValues.modelPhase2)
+    }
+  }
+}
+
+object RPValues {
+
   private val nameDtls = Some(NameDetails(PersonName(Some("name"), Some("some"), Some("surname")), Some(OthrNamesOrAliasesDetails(true, Some(Seq("Doc")))),
     Some(PreviousNameDetails(true, Some(PersonName(Some("fname"), Some("mname"), Some("lname"))), Some("1990-02-24")))))
   private val nationalDtls = Some(NationalityDetails(true, Some(IdDetail(Some(UkResident("nino")), None)), Some("GB"), Some("GB")))
@@ -40,190 +82,166 @@ class ResponsiblePersonsSpec extends PlaySpec with OneAppPerSuite {
   private val positionInBusiness = Some(PositionInBusiness(Some(SoleProprietor(true, true, Some(false))),
     None, None))
 
-  "ResponsiblePersons" should {
-    val responsiblePeople = ResponsiblePeople(
-      Some(models.fe.responsiblepeople.PersonName("name", Some("some"), "surname")),
-      Some(PreviousName(true, Some("fname"), Some("mname"), Some("lname"))),
-      Some(LocalDate.parse("1990-02-24")),
-      Some(KnownBy(true, Some("Doc"))),
-      Some(PersonResidenceType(UKResidence("nino"), "GB", "GB")),
-      None,
-      None,
-      None,
-      Some(ContactDetails("07000001122", "test@test.com")),
-      Some(ResponsiblePersonAddressHistory(
-        Some(ResponsiblePersonCurrentAddress(PersonAddressUK("ccLine 1", "ccLine 2", None, None, "AA1 1AA"), ZeroToFiveMonths, None)),
-        Some(ResponsiblePersonAddress(PersonAddressUK("Line 1", "Line 2", None, None, "BB1 1BB"),SixToElevenMonths)),
-        Some(ResponsiblePersonAddress(PersonAddressUK("e Line 1", "e Line 2", Some("e Line 3"), Some("e Line 4"), "CC1 1CC"), OneToThreeYears)))
+  val responsiblePeople = ResponsiblePeople(
+    Some(models.fe.responsiblepeople.PersonName("name", Some("some"), "surname")),
+    Some(PreviousName(true, Some("fname"), Some("mname"), Some("lname"))),
+    Some(LocalDate.parse("1990-02-24")),
+    Some(KnownBy(true, Some("Doc"))),
+    Some(PersonResidenceType(UKResidence("nino"), "GB", "GB")),
+    None,
+    None,
+    None,
+    Some(ContactDetails("07000001122", "test@test.com")),
+    Some(ResponsiblePersonAddressHistory(
+      Some(ResponsiblePersonCurrentAddress(PersonAddressUK("ccLine 1", "ccLine 2", None, None, "AA1 1AA"), ZeroToFiveMonths, None)),
+      Some(ResponsiblePersonAddress(PersonAddressUK("Line 1", "Line 2", None, None, "BB1 1BB"),SixToElevenMonths)),
+      Some(ResponsiblePersonAddress(PersonAddressUK("e Line 1", "e Line 2", Some("e Line 3"), Some("e Line 4"), "CC1 1CC"), OneToThreeYears)))
+    ),
+    Some(Positions(Set(models.fe.responsiblepeople.NominatedOfficer, models.fe.responsiblepeople.SoleProprietor), None)),
+    Some(SaRegisteredYes("0123456789")),
+    Some(VATRegisteredNo),
+    Some(ExperienceTrainingYes("Some training")),
+    Some(TrainingYes("test")),
+    Some(true),
+    None,
+    None,
+    None,
+    false
+  )
+  
+  val businessMatching = BusinessMatching(
+    activities = BusinessActivities(Set.empty),
+    reviewDetails = ReviewDetails(
+      "",
+      BusinessType.SoleProprietor,
+      models.fe.businesscustomer.Address(
+        line_1 = "",
+        line_2 = "",
+        line_3 = None,
+        line_4 = None,
+        postcode = None,
+        country = ""
       ),
-      Some(Positions(Set(models.fe.responsiblepeople.NominatedOfficer, models.fe.responsiblepeople.SoleProprietor), None)),
-      Some(SaRegisteredYes("0123456789")),
-      Some(VATRegisteredNo),
-      Some(ExperienceTrainingYes("Some training")),
-      Some(TrainingYes("test")),
-      Some(true),
-      None,
-      None,
-      None,
-      false
+      ""
     )
-    val businessMatching = BusinessMatching(
-      activities = BusinessActivities(Set.empty),
-      reviewDetails = ReviewDetails(
-        "",
-        BusinessType.SoleProprietor,
-        models.fe.businesscustomer.Address(
-          line_1 = "",
-          line_2 = "",
-          line_3 = None,
-          line_4 = None,
-          postcode = None,
-          country = ""
+  )
+
+  val model = ResponsiblePersons(
+    nameDtls,
+    nationalDtls,
+    contactDtls,
+    currentDesAddress,
+    Some("0-6 months"),
+    additionalDesAddress,
+    Some("7-12 months"),
+    extraAdditional,
+    Some("1-3 years"),
+    positionInBusiness,
+    regDtls,
+    true,
+    Some("Some training"),
+    true,
+    Some("test"),
+    None,
+    None,
+    Some(MsbOrTcsp(true)),
+    extra = RPExtra(None)
+  )
+
+  val modelPhase2 = model.copy(
+    msbOrTcsp = None,
+    passedFitAndProperTest = Some(false),
+    passedApprovalCheck = Some(true)
+  )
+
+  val jsonExpectedFromWrite = Json.obj(
+    "nameDetails" -> Json.obj(
+      "personName" -> Json.obj(
+        "firstName" -> "name",
+        "middleName" -> "some",
+        "lastName" -> "surname"
+      ),
+      "othrNamesOrAliasesDetails" -> Json.obj(
+        "otherNamesOrAliases" -> true,
+        "aliases" -> Json.arr(
+          "Doc"
+        )
+      ),
+      "previousNameDetails" -> Json.obj(
+        "nameEverChanged" -> true,
+        "previousName" -> Json.obj(
+          "firstName" -> "fname",
+          "middleName" -> "mname",
+          "lastName" -> "lname"
         ),
-        ""
+        "dateOfChange" -> "1990-02-24"
       )
-    )
-
-    val model = ResponsiblePersons(
-      nameDtls,
-      nationalDtls,
-      contactDtls,
-      currentDesAddress,
-      Some("0-6 months"),
-      additionalDesAddress,
-      Some("7-12 months"),
-      extraAdditional,
-      Some("1-3 years"),
-      positionInBusiness,
-      regDtls,
-      true,
-      Some("Some training"),
-      true,
-      Some("test"),
-      None,
-      None,
-      Some(MsbOrTcsp(true)),
-      extra = RPExtra(None)
-    )
-
-    val modelPhase2 = model.copy(
-      msbOrTcsp = None,
-      passedFitAndProperTest = Some(false),
-      passedApprovalCheck = Some(true)
-    )
-
-    val jsonExpectedFromWrite = Json.obj(
-      "nameDetails" -> Json.obj(
-        "personName" -> Json.obj(
-          "firstName" -> "name",
-          "middleName" -> "some",
-          "lastName" -> "surname"
-        ),
-        "othrNamesOrAliasesDetails" -> Json.obj(
-          "otherNamesOrAliases" -> true,
-          "aliases" -> Json.arr(
-            "Doc"
-          )
-        ),
-        "previousNameDetails" -> Json.obj(
-          "nameEverChanged" -> true,
-          "previousName" -> Json.obj(
-            "firstName" -> "fname",
-            "middleName" -> "mname",
-            "lastName" -> "lname"
-          ),
-          "dateOfChange" -> "1990-02-24"
+    ),
+    "nationalityDetails" -> Json.obj(
+      "areYouUkResident" -> true,
+      "idDetails" -> Json.obj(
+        "ukResident" -> Json.obj(
+          "nino" -> "nino"
         )
       ),
-      "nationalityDetails" -> Json.obj(
-        "areYouUkResident" -> true,
-        "idDetails" -> Json.obj(
-          "ukResident" -> Json.obj(
-            "nino" -> "nino"
-          )
-        ),
-        "countryOfBirth" -> "GB",
-        "nationality" -> "GB"
-      ),
-      "contactCommDetails" -> Json.obj(
-        "contactEmailAddress" -> "test@test.com",
-        "primaryTeleNo" -> "07000001122"
-      ),
-      "currentAddressDetails" -> Json.obj(
-        "address" -> Json.obj(
-          "addressLine1" -> "ccLine 1",
-          "addressLine2" -> "ccLine 2",
-          "country" -> "GB",
-          "postcode" -> "AA1 1AA"
-        )
-      ),
-      "timeAtCurrentAddress" -> "0-6 months",
-      "addressUnderThreeYears" -> Json.obj(
-        "address" -> Json.obj(
-          "addressLine1" -> "Line 1",
-          "addressLine2" -> "Line 2",
-          "country" -> "GB",
-          "postcode" -> "BB1 1BB"
-        )
-      ),
-      "timeAtAddressUnderThreeYears" -> "7-12 months",
-      "addressUnderOneYear" -> Json.obj(
-        "address" -> Json.obj(
-          "addressLine1" -> "e Line 1",
-          "addressLine2" -> "e Line 2",
-          "addressLine3" -> "e Line 3",
-          "addressLine4" -> "e Line 4",
-          "country" -> "GB",
-          "postcode" -> "CC1 1CC"
-        )
-      ),
-      "timeAtAddressUnderOneYear" -> "1-3 years",
-      "positionInBusiness" -> Json.obj(
-        "soleProprietor" -> Json.obj(
-          "soleProprietor" -> true,
-          "nominatedOfficer" -> true,
-          "other" -> false
-        )),
-      "regDetails" -> Json.obj(
-        "vatRegistered" -> false,
-        "saRegistered" -> true,
-        "saUtr" -> "0123456789"
-      ),
-      "previousExperience" -> true,
-      "descOfPrevExperience" -> "Some training",
-      "amlAndCounterTerrFinTraining" -> true,
-      "trainingDetails" -> "test",
-      "msbOrTcsp" -> Json.obj(
-        "passedFitAndProperTest" -> true
+      "countryOfBirth" -> "GB",
+      "nationality" -> "GB"
+    ),
+    "contactCommDetails" -> Json.obj(
+      "contactEmailAddress" -> "test@test.com",
+      "primaryTeleNo" -> "07000001122"
+    ),
+    "currentAddressDetails" -> Json.obj(
+      "address" -> Json.obj(
+        "addressLine1" -> "ccLine 1",
+        "addressLine2" -> "ccLine 2",
+        "country" -> "GB",
+        "postcode" -> "AA1 1AA"
       )
+    ),
+    "timeAtCurrentAddress" -> "0-6 months",
+    "addressUnderThreeYears" -> Json.obj(
+      "address" -> Json.obj(
+        "addressLine1" -> "Line 1",
+        "addressLine2" -> "Line 2",
+        "country" -> "GB",
+        "postcode" -> "BB1 1BB"
+      )
+    ),
+    "timeAtAddressUnderThreeYears" -> "7-12 months",
+    "addressUnderOneYear" -> Json.obj(
+      "address" -> Json.obj(
+        "addressLine1" -> "e Line 1",
+        "addressLine2" -> "e Line 2",
+        "addressLine3" -> "e Line 3",
+        "addressLine4" -> "e Line 4",
+        "country" -> "GB",
+        "postcode" -> "CC1 1CC"
+      )
+    ),
+    "timeAtAddressUnderOneYear" -> "1-3 years",
+    "positionInBusiness" -> Json.obj(
+      "soleProprietor" -> Json.obj(
+        "soleProprietor" -> true,
+        "nominatedOfficer" -> true,
+        "other" -> false
+      )),
+    "regDetails" -> Json.obj(
+      "vatRegistered" -> false,
+      "saRegistered" -> true,
+      "saUtr" -> "0123456789"
+    ),
+    "previousExperience" -> true,
+    "descOfPrevExperience" -> "Some training",
+    "amlAndCounterTerrFinTraining" -> true,
+    "trainingDetails" -> "test",
+    "msbOrTcsp" -> Json.obj(
+      "passedFitAndProperTest" -> true
     )
+  )
 
-    val jsonExpectedFromWritePhase2 = jsonExpectedFromWrite
-        .-("msbOrTcsp")
-        .+("passedFitAndProperTest", JsBoolean(false))
-        .+("passedApprovalCheck", JsBoolean(true))
-
-
-    "Serialise to json successfully" in {
-      ResponsiblePersons.jsonWrites.writes(model) must be(jsonExpectedFromWrite)
-    }
-
-    "Deserialise from json successfully" in {
-      ResponsiblePersons.jsonReads.reads(jsonExpectedFromWrite) must be (JsSuccess(model))
-    }
-
-    "Serialise to phase2 json successfully" in {
-      ResponsiblePersons.jsonWrites.writes(modelPhase2) must be(jsonExpectedFromWritePhase2)
-    }
-
-    "Deserialise from phase2 json successfully" in {
-      ResponsiblePersons.jsonReads.reads(jsonExpectedFromWritePhase2) must be (JsSuccess(modelPhase2))
-    }
-
-    "convert FE model to DES model for phase 1" in {
-      val responsiblePerson = ResponsiblePersons.convertResponsiblePeopleToResponsiblePerson(responsiblePeople, businessMatching)
-
-      responsiblePerson must be (model)
-    }
-  }
+  val jsonExpectedFromWritePhase2 = jsonExpectedFromWrite
+    .-("msbOrTcsp")
+    .+("passedFitAndProperTest", JsBoolean(false))
+    .+("passedApprovalCheck", JsBoolean(true))
 }
