1c74c8186fc7fb4f0c498558bc2eb51cd2109ec6
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2018-10-03 15:20:29 +0100

    Added new unit tests for phase 2 toggle to subscriptionviewspec + release7

--- test/models/fe/SubscriptionViewSpec.scala
+++ test/models/fe/SubscriptionViewSpec.scala
@@ -103,6 +103,83 @@ class SubscriptionViewSpec extends PlaySpec with OneAppPerSuite{
   )
 }
 
+class SubscriptionViewSpecPhase2 extends PlaySpec with OneAppPerSuite{
+
+  implicit override lazy val app = FakeApplication(
+    additionalConfiguration = Map(
+      "microservice.services.feature-toggle.release7" -> false,
+      "microservice.services.feature-toggle.phase-2-changes" -> true
+    )
+  )
+
+  "SubscriptionView" must {
+    "deserialise the subscription json if phase 2 toggle is off" when {
+      "given valid json" in {
+
+        val json = Json.toJson(GetSuccessModel)
+
+        val subscriptionViewModel = GetSuccessModel
+
+        json.as[SubscriptionView] must be(subscriptionViewModel)
+
+        Json.toJson(GetSuccessModel) must be(json)
+      }
+
+      "convert des model to frontend model" in {
+
+        SubscriptionView.convert(DesConstants.SubscriptionViewModelForRpPhase2) must be(SubscriptionViewModel.convertedViewModelPhase2)
+      }
+
+      "convert des model correctly to include fit and proper answer" in {
+
+        SubscriptionView.convert(DesConstants.SubscriptionViewModelForRpPhase2.copy(responsiblePersons = Some(DesConstants.testResponsiblePersonsForRp.map {
+          rp => rp.copy(msbOrTcsp = None)
+        }))) must be(SubscriptionViewModel.convertedViewModelPhase2.copy(
+          responsiblePeopleSection = SubscriptionViewModel.convertedViewModelPhase2.responsiblePeopleSection match {
+            case None => None
+            case Some(rpSeq) => Some(rpSeq.map {
+              rp => rp.copy(approvalFlags = rp.approvalFlags.copy(hasAlreadyPassedFitAndProper = Some(false)))
+            })
+          }
+        ))
+      }
+
+      "convert des model correctly to include fit and proper answer when only msb" in {
+
+        SubscriptionView.convert(DesConstants.SubscriptionViewModelForRpPhase2.copy(responsiblePersons = Some(DesConstants.testResponsiblePersonsForRp.map {
+          rp => rp.copy(msbOrTcsp = None)
+        }), businessActivities = DesConstants.testBusinessActivities.copy(
+          mlrActivitiesAppliedFor = Some(MlrActivitiesAppliedFor(true, false, false, false, false, false, false))))) must be(
+          SubscriptionViewModel.convertedViewModelPhase2.copy(
+            responsiblePeopleSection = SubscriptionViewModel.convertedViewModelPhase2.responsiblePeopleSection match {
+              case None => None
+              case Some(rpSeq) => Some(rpSeq.map {
+                rp => rp.copy(approvalFlags = rp.approvalFlags.copy(hasAlreadyPassedFitAndProper = Some(false)))
+              })
+            }
+            ,businessMatchingSection = SubscriptionViewModel.convertedViewModelPhase2.businessMatchingSection.copy(activities = BusinessActivities(Set(MoneyServiceBusiness)))))
+      }
+    }
+  }
+
+  val GetSuccessModel = SubscriptionView(
+    etmpFormBundleNumber = "111111",
+    businessMatchingSection = BusinessMatchingSection.model,
+    eabSection = EabSection.model,
+    aboutTheBusinessSection = AboutTheBusinessSection.model,
+    tradingPremisesSection = TradingPremisesSection.model,
+    bankDetailsSection = BankDetailsSection.model,
+    aboutYouSection = AboutYouSection.model,
+    businessActivitiesSection = BusinessActivitiesSection.model,
+    responsiblePeopleSection = ResponsiblePeopleSection.model,
+    tcspSection = ASPTCSPSection.TcspSection,
+    aspSection = ASPTCSPSection.AspSection,
+    msbSection = MsbSection.completeModel,
+    hvdSection = HvdSection.completeModel,
+    supervisionSection = SupervisionSection.completeModel
+  )
+}
+
 class SubscriptionViewSpecRelease7 extends PlaySpec with OneAppPerSuite {
 
   implicit override lazy val app = FakeApplication(
@@ -205,3 +282,107 @@ class SubscriptionViewSpecRelease7 extends PlaySpec with OneAppPerSuite {
   )
 
 }
+
+class SubscriptionViewSpecRelease7Phase2 extends PlaySpec with OneAppPerSuite {
+
+  implicit override lazy val app = FakeApplication(
+    additionalConfiguration = Map(
+      "microservice.services.feature-toggle.release7" -> true,
+      "microservice.services.feature-toggle.phase-2-changes" -> true
+    )
+  )
+
+  val agentDetails = DesConstants.testTradingPremisesAPI5.agentBusinessPremises.fold[Option[Seq[AgentDetails]]](None){
+    x => x.agentDetails match {
+      case Some(data) => Some(data.map(y => y.copy(agentPremises = y.agentPremises.copy(startDate = None),
+        startDate = y.agentPremises.startDate)))
+      case _ => None
+    }
+  }
+
+  val release7SubscriptionViewModel = DesConstants.SubscriptionViewModelForRpPhase2.copy(businessActivities = DesConstants.testBusinessActivities.copy(
+    all = Some(DesConstants.testBusinessActivitiesAll.copy(
+      businessActivityDetails = BusinessActivityDetails(true, Some(ExpectedAMLSTurnover(Some("£50k-£100k"))))
+    ))
+  ), tradingPremises = DesConstants.testTradingPremisesAPI5.copy(agentBusinessPremises = Some(AgentBusinessPremises(true, agentDetails))),
+    msb = Some(DesConstants.testMsb.copy(
+      msbAllDetails = Some(MsbAllDetails(
+        Some("£50k-£100k"),
+        true,
+        Some(CountriesList(List("AD", "GB"))),
+        true)
+      ))))
+
+  "SubscriptionView" must {
+    "deserialise the subscription json when phase 2 toggle is off" when {
+      "given valid json" in {
+
+        val json = Json.toJson(GetSuccessModel)
+
+        val subscriptionViewModel = GetSuccessModel
+
+        json.as[SubscriptionView] must be(subscriptionViewModel)
+
+        Json.toJson(GetSuccessModel) must be(json)
+      }
+
+      "convert des model to frontend model" in {
+
+        SubscriptionView.convert(release7SubscriptionViewModel) must be(SubscriptionViewModel.convertedViewModelPhase2)
+      }
+
+      "convert des model correctly to include fit and proper answer" in {
+
+        SubscriptionView.convert(release7SubscriptionViewModel.copy(responsiblePersons = Some(DesConstants.testResponsiblePersonsForRp.map {
+          rp => rp.copy(msbOrTcsp = None)
+        }))) must be(SubscriptionViewModel.convertedViewModelPhase2.copy(
+          responsiblePeopleSection = SubscriptionViewModel.convertedViewModelPhase2.responsiblePeopleSection match {
+            case None => None
+            case Some(rpSeq) => Some(rpSeq.map {
+              rp => rp.copy(approvalFlags = rp.approvalFlags.copy(hasAlreadyPassedFitAndProper = Some(false)))
+            })
+          }
+        ))
+      }
+
+      "convert des model correctly to include fit and proper answer when only msb" in {
+
+        SubscriptionView.convert(release7SubscriptionViewModel.copy(responsiblePersons = Some(DesConstants.testResponsiblePersonsForRp.map {
+          rp => rp.copy(msbOrTcsp = None)
+        }), businessActivities = DesConstants.testBusinessActivities.copy(
+          mlrActivitiesAppliedFor = Some(MlrActivitiesAppliedFor(true, false, false, false, false, false, false)),
+          all = Some(DesConstants.testBusinessActivitiesAll.copy(
+            businessActivityDetails = BusinessActivityDetails(true, Some(ExpectedAMLSTurnover(Some("£50k-£100k"))))
+          )) ))) must be(
+          SubscriptionViewModel.convertedViewModelPhase2.copy(
+            responsiblePeopleSection = SubscriptionViewModel.convertedViewModelPhase2.responsiblePeopleSection match {
+              case None => None
+              case Some(rpSeq) => Some(rpSeq.map {
+                rp => rp.copy(approvalFlags = rp.approvalFlags.copy(hasAlreadyPassedFitAndProper = Some(false)))
+              })
+            }
+            ,businessMatchingSection = SubscriptionViewModel.convertedViewModelPhase2.businessMatchingSection.copy(
+              activities = BusinessActivities(Set(MoneyServiceBusiness)))))
+      }
+    }
+  }
+
+  val GetSuccessModel = SubscriptionView(
+    etmpFormBundleNumber = "111111",
+    businessMatchingSection = BusinessMatchingSection.model,
+    eabSection = EabSection.model,
+    aboutTheBusinessSection = AboutTheBusinessSection.model,
+    tradingPremisesSection = TradingPremisesSection.model,
+    bankDetailsSection = BankDetailsSection.model,
+    aboutYouSection = AboutYouSection.model,
+    businessActivitiesSection = BusinessActivitiesSection.model,
+    responsiblePeopleSection = ResponsiblePeopleSection.model,
+    tcspSection = ASPTCSPSection.TcspSection,
+    aspSection = ASPTCSPSection.AspSection,
+    msbSection = MsbSection.completeModel,
+    hvdSection = HvdSection.completeModel,
+    supervisionSection = SupervisionSection.completeModel
+  )
+
+}
+
