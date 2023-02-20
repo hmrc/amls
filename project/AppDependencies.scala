b717efd66a9daf93e882bbe218cc21d99b3fab9e
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2023-02-20 09:29:27 +0000

    DDCYLS-2315
    Removed dependencies:
    uk.gov.hmrc domain
    de.flapdoodle.embed mongo
    com.github.kxbmap configs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,11 @@ private object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"         % "0.74.0",
-    "uk.gov.hmrc"         %% "domain"                     % "6.2.0-play-28",
     "org.typelevel"       %% "cats"                       % "0.9.0",
     "com.eclipsesource"   %% "play-json-schema-validator" % "0.9.4",
-    "de.flapdoodle.embed" %  "de.flapdoodle.embed.mongo"  % "2.2.0" % "test,it",
     "com.beachape"        %% "enumeratum"                 % "1.7.0",
     "uk.gov.hmrc"         %% "bootstrap-backend-play-28"  % "5.12.0",
     "com.typesafe.play"   %% "play-json-joda"             % "2.7.4",
-    "com.github.kxbmap"   %% "configs"                    % "0.6.1",
     "com.github.ghik"     %  "silencer-lib"               % "1.7.11" % Provided cross CrossVersion.full,
     compilerPlugin("com.github.ghik" % "silencer-plugin"  % "1.7.11" cross CrossVersion.full)
   )
