3239b6d31d5f9fb28a611833e7df2a21a5662994
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2024-01-19 12:23:45 +0000

    DDCYLS-4024
    Upgrading to Play 3.0 & Apache Pekko
    Removing eclipse json validator & using the one from com.github.fge
    Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,15 @@ private object AppDependencies {
 
   val bootstrapVersion = "8.4.0"
 
+  private val playV = "play-30"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"         % "1.7.0",
+    "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playV"         % "1.7.0",
     "org.typelevel"        %% "cats-core"                  % "2.10.0",
     "com.github.fge"       %  "json-schema-validator"      % "2.2.14",
-    "com.eclipsesource"    %% "play-json-schema-validator" % "0.9.5",
     "com.beachape"         %% "enumeratum"                 % "1.7.3",
-    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"  % bootstrapVersion
+    "uk.gov.hmrc"          %% s"bootstrap-backend-$playV"  % bootstrapVersion
   )
 
   trait TestDependencies {
@@ -25,9 +26,8 @@ private object AppDependencies {
       override val test: Seq[sbt.ModuleID] = Seq(
         "org.mockito"             %% "mockito-scala"             % "1.17.30"                % scope,
         "org.scalatestplus"       %% "scalacheck-1-17"           % "3.2.17.0"               % scope,
-        "uk.gov.hmrc"             %% "bootstrap-test-play-28"    % bootstrapVersion         % scope,
-        "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"   % "1.7.0"                  % scope,
-        "com.vladsch.flexmark"    %  "flexmark-all"              % "0.64.8"                 % scope
+        "uk.gov.hmrc"             %% s"bootstrap-test-$playV"    % bootstrapVersion         % scope,
+        "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playV"   % "1.7.0"                  % scope,
       )
     }.test
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,10 +8,10 @@ ThisBuild / libraryDependencySchemes ++= Seq(
   "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 )
 
-addSbtPlugin("uk.gov.hmrc"        %  "sbt-auto-build"         % "3.18.0")
+addSbtPlugin("uk.gov.hmrc"        %  "sbt-auto-build"         % "3.20.0")
 addSbtPlugin("com.github.gseitz"  %  "sbt-release"            % "1.0.13")
-addSbtPlugin("com.typesafe.play"  %  "sbt-plugin"             % "2.8.21")
-addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.4.0")
+addSbtPlugin("org.playframework"  %  "sbt-plugin"             % "3.0.1")
+addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.5.0")
 addSbtPlugin("org.scoverage"      %% "sbt-scoverage"          % "2.0.9")
 addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin"  % "1.0.0")
 addSbtPlugin("net.virtual-void"   %  "sbt-dependency-graph"   % "0.9.2")
