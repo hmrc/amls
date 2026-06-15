0d017b5ad5fbe43c5573bba2ca0e9273dcb6ba50
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2026-06-15 12:46:48 +0100

    DDCYLS-8178 Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,15 +12,15 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playV"         % hmrcMongoVersion,
-    "at.yawk.lz4"          %  "lz4-java"                   % "1.10.3",
-    "org.typelevel"        %% "cats-core"                  % "2.10.0",
+    "at.yawk.lz4"          %  "lz4-java"                   % "1.11.0",
+    "org.typelevel"        %% "cats-core"                  % "2.13.0",
     "com.github.fge"       %  "json-schema-validator"      % "2.2.14" exclude("org.mozilla", "rhino"),
-    "org.mozilla"          %  "rhino"                      % "1.8.1",
-    "ch.qos.logback"       % "logback-core"                % "1.5.27",
-    "ch.qos.logback"       % "logback-classic"             % "1.5.27",
-    "com.beachape"         %% "enumeratum"                 % "1.7.3",
+    "org.mozilla"          %  "rhino"                      % "1.9.1",
+    "ch.qos.logback"       % "logback-core"                % "1.5.34",
+    "ch.qos.logback"       % "logback-classic"             % "1.5.34",
+    "com.beachape"         %% "enumeratum"                 % "1.9.8",
     "uk.gov.hmrc"          %% s"bootstrap-backend-$playV"  % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
-    "org.apache.commons" % "commons-lang3" % "3.18.0"
+    "org.apache.commons"   % "commons-lang3"               % "3.20.0"
   )
 
   trait TestDependencies {
@@ -31,11 +31,11 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[sbt.ModuleID] = new TestDependencies {
       override val test: Seq[sbt.ModuleID] = Seq(
-        "org.mockito"             %% "mockito-scala"             % "1.17.30"                % scope,
-        "org.scalatestplus"       %% "scalacheck-1-17"           % "3.2.17.0"               % scope,
-        "uk.gov.hmrc"             %% s"bootstrap-test-$playV"    % bootstrapVersion         % scope,
-        "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playV"   % hmrcMongoVersion                  % scope,
-        "org.scalamock" %% "scalamock" % "5.2.0" % scope
+        "org.mockito"             %% "mockito-scala"             % "2.2.1"                % scope,
+        "org.scalatestplus"       %% "scalacheck-1-17"           % "3.2.18.0"             % scope,
+        "uk.gov.hmrc"             %% s"bootstrap-test-$playV"    % bootstrapVersion       % scope,
+        "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playV"   % hmrcMongoVersion       % scope,
+        "org.scalamock"           %% "scalamock"                 % "7.5.5"                % scope
       )
     }.test
   }
